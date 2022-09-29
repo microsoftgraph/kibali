f694e9cfca4320ad7e2cc1e3cf41b694b3ad8935
James Maringa <james.maringa@microsoft.com>
2022-09-29 16:29:18 +0300

    Clean up serialization and tests

--- src/kibali/Permission.cs
+++ src/kibali/Permission.cs
@@ -8,14 +8,9 @@ namespace Kibali
     {
         public string Note { get; set; }
         public bool Implicit { get; set; } = false;
-        public bool IsHidden { get; set; } = false;
-        public string OwnerEmail { get; set; }
         public string PrivilegeLevel { get; set; }
-        public List<string> RequiredEnvironments { get; set; } = new List<string>();
-
         public Dictionary<string, Scheme> Schemes { get; set; } = new Dictionary<string, Scheme>();
         public List<PathSet> PathSets { get; set; } = new List<PathSet>();
-
         public ProvisioningInfo ProvisioningInfo { get; set; } = new();
 
         public void Write(Utf8JsonWriter writer)
@@ -24,8 +19,6 @@ namespace Kibali
 
             if (!String.IsNullOrWhiteSpace(Note)) writer.WriteString("note", Note);
             if (Implicit == true) writer.WriteBoolean("implicit", Implicit);
-            if (IsHidden == true) writer.WriteBoolean("isHidden", IsHidden);
-            if (!String.IsNullOrWhiteSpace(OwnerEmail)) writer.WriteString("ownerEmail", OwnerEmail);
 
             writer.WritePropertyName("schemes");
             writer.WriteStartObject();
@@ -61,10 +54,7 @@ namespace Kibali
         {
             { "note", (o,v) => {o.Note = v.GetString();  } },
             { "privilegeLevel", (o,v) => {o.PrivilegeLevel= v.GetString();  } },
-            { "ownerEmail", (o,v) => {o.OwnerEmail= v.GetString();  } },
-            { "requiredEnvironments", (o,v) => {o.RequiredEnvironments= ParsingHelpers.GetListOfString(v); } },
             { "implicit", (o,v) => {o.Implicit = v.GetBoolean();  } },
-            { "isHidden", (o,v) => {o.IsHidden = v.GetBoolean();  } },
             { "pathSets", (o,v) => {o.PathSets = ParsingHelpers.GetList(v, PathSet.Load);  } },
             { "schemes", (o,v) => {o.Schemes = ParsingHelpers.GetMap(v, Scheme.Load);  } },
         };
