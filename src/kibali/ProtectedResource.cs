bc5e8cee0a9a55ec95c547d1b8187fdcecbef319
James Maringa <james.maringa@microsoft.com>
2024-08-13 08:19:06 +0300

    Remove unnecessary import

--- src/kibali/ProtectedResource.cs
+++ src/kibali/ProtectedResource.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Kibali
 {
